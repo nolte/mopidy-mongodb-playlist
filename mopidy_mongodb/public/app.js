1114e08bf1424311b20a3a02d02fef107822fd09
nolte <nolte@ingo.de>
2016-07-31 00:32:54 +0200

    rebase angular app

--- mopidy_mongodb/public/app.js
+++ mopidy_mongodb/public/app.js
@@ -1,38 +1,70 @@
-angular.module('api', [
-    'controllers'
-]);
-angular.module('controllers', ['mgcrea.ngStrap'])
-    .controller('MainCtrl', function ($scope) {
-        window.mopidy = new Mopidy({callingConvention: "by-position-or-by-name"});
-        console.log('from inside the log method rock on' );
-        $scope.methods_toc = {}
-        mopidy.on("state:online", function () {
-        	  mopidy._send({method: "core.playlists.as_list" }).then(function (data) {
-                  window.a = data;
-                  $scope.$apply(function () {
-	                  $scope.playlists = data;
-	                  data.forEach(function (playlist) {
-	                	  if(playlist.uri.toLowerCase().startsWith('mongod'))
-	                	  {
-	                		  console.log('element: ' + JSON.stringify(playlist) );  
-	                	  }
-	                  });
-                  });
-                  
-              });
-        });
-    }).filter('htmlify', ['$sce', function ($sce) {
-        return function (input) {
-            if (!input) {
-                return '';
-            }
-            return $sce.trustAsHtml(input);
-        };
-    }]);
+'use strict';
+
+angular
+		.module('playlist-manager', ['angularValidator', 'ngAnimate', 'ngRoute' ])
+		.config(function($routeProvider) {
+			$routeProvider.when('/', {
+				templateUrl : 'articles.html'
+			}).when('/about', {
+				template : 'Über unsere Pizzeria'
+			}).when('/create', {
+				templateUrl : 'create.html'
+			}).otherwise({
+				redirectTo : '/'
+			});
+		})
+		.directive(
+				'price',
+				function() {
+					return {
+						restrict : 'E',
+						scope : {
+							value : '='
+						},
+						template : '<span ng-show="value == 0">kostenlos</span>'
+								+ '<span ng-show="value > 0">{{value | currency}}</span>'
+					}
+				}).factory('Cart', function() {
+			var items = [];
+			return {
+				getItems : function() {
+					return items;
+				},
+				addArticle : function(article) {
+					items.push(article);
+				},
+				sum : function() {
+					return items.reduce(function(total, article) {
+						return total + article.price;
+					}, 0);
+				}
+			};
+		}).controller('ArticlesCtrl', function($scope, $http, Cart) {
+			$scope.cart = Cart;
+			$http.get('articles.json').then(function(articlesResponse) {
+				$scope.articles = articlesResponse.data;
+			});
+		}).controller('CartCtrl', function($scope, Cart) {
+			$scope.cart = Cart;
+		}).controller('CreateCtrl', ['$scope','$location','$log','$route',function($scope, $location,$log,$route) {
+			$scope.create = function(playlist) {
+				window.mopidy = new Mopidy({callingConvention: "by-position-or-by-name"});
+				$log.debug('create new Playlist', playlist);
+		        mopidy.on("state:online", function () {
+		        	  mopidy._send({method: "core.playlists.create",jsonrpc: "2.0", params: playlist, id: 1 }).then(function (data) {
+		        		  $log.debug('response: ', data);
+		        		  $location.path('/about');
+		        		  $route.reload();
+		              });
+		        });
+			};
+
+		}]);
+
 var ws = new WebSocket("ws://" + document.location.host + "/mopidy/ws/");
 ws.onmessage = function (message) {
-    var console = document.getElementById('ws-console');
-    var newLine = (new Date()).toLocaleTimeString() + ": " +
-        message.data + "\n";
-    console.innerHTML = newLine + console.innerHTML;
+    // var console = document.getElementById('ws-console');
+	// var newLine = (new Date()).toLocaleTimeString() + ": " +
+	//   message.data + "\n";
+	// console.innerHTML = newLine + console.innerHTML;
 };
