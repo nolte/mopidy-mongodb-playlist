5dc507f1677a196353a471f8eba677069030ab89
nolte <nolte@ingo.de>
2016-07-30 17:04:59 +0200

    format

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -4,26 +4,24 @@ import unittest
 
 from mopidy_mongodb import Extension
 
+
 class ExtensionTest(unittest.TestCase):
 
     def test_get_default_config(self):
         ext = Extension()
-    
+
         config = ext.get_default_config()
 
         self.assertIn('[mongodb]', config)
         self.assertIn('enabled = true', config)
-        self.assertIn('host = john', config)    
-        self.assertIn('port = 27017', config)            
-    
-    
+        self.assertIn('host = john', config)
+        self.assertIn('port = 27017', config)
+
     def test_get_config_schema(self):
         ext = Extension()
-    
+
         schema = ext.get_config_schema()
-    
+
         # Test the content of your config schema
         self.assertIn('host', schema)
         self.assertIn('port', schema)
-
-
