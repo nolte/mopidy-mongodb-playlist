5b92bdb2748b931e7907ded47b7ac726c755ecbb
nolte <nolte@ingo.de>
2016-07-30 16:48:26 +0200

    update tests

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-curl -X POST -H Content-Type:application/json -d '{
-  "method": "core.playlists.create",
-  "jsonrpc": "2.0",
-  "params": {
-    "name": "myTest",
-    "uri_scheme": "mongodb:firstplaylist"
-  },
-  "id": 1
-}' http://localhost:6680/mopidy/rpc
--- tests/test_extension.py
+++ tests/test_extension.py
@@ -1,28 +1,29 @@
 from __future__ import unicode_literals
 
-from mopidy_mongodb import Extension, frontend as frontend_lib
+import unittest
+
+from mopidy_mongodb import Extension
+
+class ExtensionTest(unittest.TestCase):
+
+    def test_get_default_config():
+        ext = Extension()
+    
+        config = ext.get_default_config()
+
+        self.assertIn('[mongodb]', config)
+        self.assertIn('enabled = true', config)
+        self.assertIn('host = john', config)    
+        self.assertIn('port = 27017', config)            
+    
+    
+    def test_get_config_schema():
+        ext = Extension()
+    
+        schema = ext.get_config_schema()
+    
+        # Test the content of your config schema
+        self.assertIn('host', schema)
+        self.assertIn('port', schema)
 
 
-def test_get_default_config():
-    ext = Extension()
-
-    config = ext.get_default_config()
-
-    assert '[mongodb]' in config
-    assert 'enabled = true' in config
-    assert 'host = john' in config
-    assert 'port = 27017' in config
-
-
-def test_get_config_schema():
-    ext = Extension()
-
-    schema = ext.get_config_schema()
-
-    # TODO Test the content of your config schema
-    assert 'host' in schema
-    assert 'port' in schema
-    #assert 'password' in schema
-
-
-# TODO Write more tests
