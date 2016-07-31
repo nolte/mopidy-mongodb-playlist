c8387089d0d5578e55a2b4dd3dc1426f1b6c3135
nolte <nolte@ingo.de>
2016-07-31 18:22:14 +0200

    save current tracklist as new playlist

--- mopidy_mongodb/public/app.js
+++ mopidy_mongodb/public/app.js
@@ -9,12 +9,54 @@ angular
 				template : 'Über unsere Pizzeria'
 			}).when('/create', {
 				templateUrl : 'create.html'
-			}).when('/playlist', {
-				templateUrl : 'playlist.html'
+			}).when('/createFromTracklist', {
+				templateUrl : 'playlistFromTracklist.html'
+			}).when('/tracklist', {
+				templateUrl : 'tracklist.html'
 			}).otherwise({
 				redirectTo : '/'
 			});
-		})
+		}).factory("mopidyCall", function () {
+			var callMopidy = function (callbackFn){
+				window.mopidy = new Mopidy({callingConvention: "by-position-or-by-name"});
+				mopidy.on("state:online", function () {
+					callbackFn(mopidy);
+			    });
+			};
+			return {
+				callMopidy: callMopidy
+		     };
+		 }).factory("playlistService", function ($log,mopidyCall) {
+			var createPlaylist = function (playlist,callbackFn){
+				mopidyCall.callMopidy(function(mopidy){
+					mopidy._send({method: "core.playlists.create",jsonrpc: "2.0", params: playlist, id: 1 }).then(function (data) {
+			       		callbackFn(data);
+			         });
+				});
+			};
+			var savePlaylist = function (playlist,callbackFn){
+				mopidyCall.callMopidy(function(mopidy){
+					$log.debug("try to save playlist ",playlist)
+					mopidy._send({method: "core.playlists.save",jsonrpc: "2.0", params: { playlist : playlist }, id: 1 }).then(function (data) {
+			       		callbackFn(data);
+			         });
+				});
+			};			
+			return {
+				createPlaylist: createPlaylist,
+				savePlaylist : savePlaylist
+		     };
+		 }).factory("currentTracklistService", function (mopidyCall) {
+			var getTracks = function (callbackFn){
+				mopidyCall.callMopidy(function(mopidy){
+			       	  mopidy._send({method: "core.tracklist.get_tracks",jsonrpc: "2.0", id: 1 }).then(function (data) {
+			       		callbackFn(data);
+			             });
+			})};
+			return {
+				getTracks: getTracks
+		     };
+		 })
 		.directive(
 				'price',
 				function() {
@@ -61,7 +103,7 @@ angular
 		        });
 			};
 
-		}]).controller('CurrentPlaylistCtrl', ['$scope','$log','$route',function($scope, $log,$route) {
+		}]).controller('CurrentTracklistCtrl', ['$scope','$log','$route',function($scope, $log,$route) {
 			window.mopidy = new Mopidy({callingConvention: "by-position-or-by-name"});
 			$log.debug('load trackist');
 	        mopidy.on("state:online", function () {
@@ -74,6 +116,25 @@ angular
 	        		  // $location.path('/about');
 	              });
 	        });
+		}]).controller('playlistFromTracklistCtrl', ['$scope','$log','$route','currentTracklistService','playlistService',function($scope, $log,$route,currentTracklistService,playlistService) {
+			window.mopidy = new Mopidy({callingConvention: "by-position-or-by-name"});
+			currentTracklistService.getTracks(function(tracks) {
+				    $scope.tracks = tracks;
+				  });
+			$scope.playlist = { uri_scheme: "mongodb" };
+			$log.debug('playlistFromTracklistCtrl');
+			$scope.create = function(playlist) {
+				playlistService.createPlaylist(playlist,function(data){
+					data.tracks = $scope.tracks;
+					$log.debug('tracklist saved',data);
+					playlistService.savePlaylist(data,function(savedPlaylist){
+						$log.debug('playlist saved',savedPlaylist);
+					})
+					
+				})
+				$log.debug('create from tracklist',$scope.tracks);
+			};
+			
 		}]);
 
 var ws = new WebSocket("ws://" + document.location.host + "/mopidy/ws/");
